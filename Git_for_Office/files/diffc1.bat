f34f218daa6a14b751831f669957680488961250
Abhijit Roy <dalvath3700@gmail.com>
2019-04-03 00:17:03 +0530

    added - showc, diffc1, diffc2

--- Git_for_Office/files/diffc1.bat
+++ Git_for_Office/files/diffc1.bat
@@ -1,5 +1,5 @@
 @echo off
-echo NOTE: This program is valid only for EXCEL, WORD files
+echo Only valid for EXCEL, WORD files
 echo,
 rem ___________________________________________________
 rem The program is for showing Excel file on a particular commit
@@ -27,11 +27,15 @@ if exist %file% (
 	rem Git Command for saving the excel file as temmporary excel file
 	git show %commit1%:%file% > temp_%commit1%_%file%
 
-	rem Open tempfile using MS Excel
-	excel temp_%commit1%_%file%
+	rem give the paths of - "temp_commit_Book1.xlsx" & "Book1.xlsx" into "tmp.txt"
+	dir temp_%commit1%_%file% %file% /b /s > tmp.txt
+
+	rem Open tempfile - "tmp.txt" using Spreadsheetcompare
+	spreadsheetcompare tmp.txt
 
 	rem Delete tempfile after closing Excel Application
 	del temp_%commit1%_%file%
+	del tmp.txt
 
 ) else (
 	msg * "Sorry! The File does not exist!"
@@ -44,8 +48,8 @@ if exist %file% (
 	rem Git Command for saving the excel file as temmporary excel file
 	git show %commit1%:%file% > temp_%commit1%_%file%
 
-	rem Open tempfile using MS Excel
-	winword temp_%commit1%_%file%
+	rem Open "tempfile" & "original file" using MS Word
+	winword temp_%commit1%_%file% %file%
 
 	rem Delete tempfile after closing Excel Application
 	del temp_%commit1%_%file%
